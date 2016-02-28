8e7ba51ddea42b518e2a38e13264c33ba3e4327c
Luis Gómez <luis@macmini-160.local>
2016-02-28 21:10:31 +0100

    Pidiendo el ISBN desde el botón search

--- PeticionISBN/ViewController.swift
+++ PeticionISBN/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var isbnIntroducido: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func searchISBN(sender: AnyObject) {
+        
+        let urls1:String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        let urls2 = self.isbnIntroducido.text!
+        
+        let urls = urls1+urls2
+        
+       // print(urls2)
+       // print(urls)
+        
+        
+        
+        let url = NSURL(string: urls)
+        let sesion = NSURLSession.sharedSession()
+        let bloque = { (datos: NSData?, resp:NSURLResponse?, error: NSError?) -> Void in let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            print(texto!)
+        }
+        
+        let dt = sesion.dataTaskWithURL(url!, completionHandler: bloque)
+       dt.resume()
+        
+        
+        
+        
+    }
+    func clearISBN(sender: AnyObject) {
+        
+    }
 }
 
