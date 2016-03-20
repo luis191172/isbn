4e8ae6f2f59a2d40db3fa424b8e2257c35fc3e54
Luis Gómez <luis@macmini-160.local>
2016-03-20 23:45:31 +0100

    Actualización del ejercicio a la semana 2

--- PeticionISBN/ViewController.swift
+++ PeticionISBN/ViewController.swift
@@ -14,16 +14,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var isbnIntroducido: UITextField! //Variable que captura el ISBN Introducido por el usuario
     
-    @IBOutlet weak var resultadoIsbn: UITextView!  //Outlet de TextView
+  
     
     override func viewWillAppear(animated: Bool) {
-        self.resultadoIsbn.text = "Aqui aparecerá el Resultado de tu Búsqueda" //Valor inicial del TextView
+      //  self.resultadoIsbn.text = "Aqui aparecerá el Resultado de tu Búsqueda" //Valor inicial del TextView
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         isbnIntroducido.delegate=self
+        
+     
+        
+        
     }
     
     @IBAction func backgroundTap(sender:UIControl){  //Funcion que esconde el teclado al tocar en el fondo
@@ -40,7 +44,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    
+    @IBOutlet weak var titulo: UITextField!
+    @IBOutlet weak var autor: UITextView!
+    @IBOutlet weak var portada: UITextField!
+    @IBOutlet weak var imagenPortada: UIImageView!
     @IBAction func isbnTecleado(sender: AnyObject){  //Funcion de busqueda con la tecla Intro
         
         
@@ -53,31 +60,66 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         if datos == nil {   //Si no hay conexion con internet la variable datos estará vacia
             
-            pintaResultado = "Error de Conexion!!"
+            self.titulo.text = "Error de Conexion!!"
+            self.autor.text = "Error de Conexion!!"
+            self.portada.text = "Error de Conexion!!"
         }else {               //Si hay conexion a internet la variable datos no estará vacia
+            titulo.text = nil
+            autor.text  = nil
+            portada.text = nil
             
        
             let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding)
             print(texto!)
                 if texto == "{}" || texto == ""{   //Miramos si el ISBN produce una búsqueda vacia...
-                    pintaResultado = "ISBN Incorrecto"
-            
+                    self.titulo.text = "ISBN Incorrecto"
+                    self.autor.text = "ISBN Incorrecto"
+                    self.portada.text = "ISBN Incorrecto"
         
                     }
          
                 else{                                  //Aqui muestra los datos en pantalla cuando el ISBN es correcto
                     pintaResultado = String(texto)
+                    
+                    do{
+                        let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                        let dico1 = json as! NSDictionary
+                        let dico2 = dico1["ISBN:\(urlsIsbnIntroducido)"] as! NSDictionary
+                        self.titulo.text = dico2["title"] as! NSString as String
+                        let dico3 = dico2["authors"] as! NSArray
+                        
+                    var texto : [String] = [""]
+                            texto.removeAll()
+                        
+                        for var i = 0; i < dico3.count; i = i+1 {
+                            
+                          let dico4 = dico3[i] as! NSDictionary
+                            texto.append(String (dico4["name"] as! NSString as String))
+                            
+                            self.autor.text = String(texto)
+                        
+                              }
+
+                                  }
+                    
+                    catch _ {
+                        
+                        
+                        
+                    }
                 }
-        resultadoIsbn.text = String(pintaResultado)
+       // resultadoIsbn.text = String(pintaResultado)
         
         }
-        resultadoIsbn.text = String(pintaResultado)
+       // resultadoIsbn.text = String(pintaResultado)
     }
         
     @IBAction func clearIsbn(sender: AnyObject) {    //Funcion del Boton "Clear"
         
         self.isbnIntroducido.text = ""
-        self.resultadoIsbn.text = "Prueba con otro ISBN!!"
+        self.titulo.text = ""
+        self.autor.text = ""
+        self.portada.text = ""
     }
 
     
